7a9a3baa875d0e1735db0d8e3069e78c520241ae
nathan-yan <nathancy@uw.edu>
2021-06-28 12:52:06 -0700

    restructured json to be more generic format

---
+++ data_.js
@@ -0,0 +1,208 @@
+const data = {
+    2020: {
+        stats: {
+            total: {
+                acceptanceRate: 97.6,
+
+                genderMale: 58.8,
+                genderFemale: 34.8,
+                genderNonBinary: 1.99,
+                genderDidNotSpecify: 4.35,
+
+                yearFreshman: 12.69,
+                yearHighSchool: 2.29,
+                yearSophomore: 26.36,
+                yearJunior: 36.6,
+                yearSenior: 23.2,
+
+                experienceFirstTime: 36.15,
+                experienceBeginner: 42.14,
+                experienceIntermediate: 20.6
+            }, 
+            uw: {
+            }
+        },
+
+        attendees: {
+            school: {
+                "UW-Seattle": 46.7,
+                "UW-Bothell": 3.5,
+                "UW-Tacoma": 1.51,
+                "U British Columbia": 6.2,
+                "U Waterloo": 2
+            },
+
+            totalEthnicity: {
+                "White": 10.64,
+                "Black": 2.4,
+                "Latinx/Hispanic": 3,
+                "East Asian" : 33.5,
+                "Southeast Asian" : 11.7,
+                "South Asian": 17.6,
+                "Middle Eastern/North African": 2.54,
+                "Hawaiian or Pacific Islander": 0.18,
+                "Mixed": 6.53, 
+                "Other": 1.9,
+                "Did not specify": 10,
+            },
+
+            totalMajor: {
+                "Computer Science": 59.3,
+                "Informatics" : 6,
+                "Electrical Engineering": 11.9,
+                "Data Science" : 9.67,
+                "Business" : 5.68,
+                "Design" : 5.56
+            },
+
+            totalCareerInterest: {
+                "Software Engineering": 80.7,
+                "Data Science": 34.34, 
+                "Entrepreneurship": 23.22,
+                "Design" : 33.3,
+                "PM": 29.38,
+                "Consulting": 10.5
+            }
+        }
+    },
+
+    2019: {
+        stats: {
+            total: {
+                numApplicants: 1602,
+                acceptanceRate: 65,
+
+                genderMale: 60,
+                genderFemale: 33,
+                genderNonBinary: 7,
+
+                yearFreshman: 15,
+                yearSophomore: 29,
+                yearJunior: 33,
+                yearSenior: 24,
+
+                studentFirstGen: 21,
+                studentReturning: 80
+            },
+
+            uw: {
+                acceptanceRate: 85, 
+
+                genderMale: 58,
+                genderFemale: 33,
+                genderNonBinary: 9,
+            }
+        },
+
+        attendees: {
+            school: {
+                "University of Washington": 55,
+                "University of British Columbia": 7,
+                "UC Berkeley": 3,
+                "Washington State University" : 3,
+                "University of California-Irvine": 2
+            },
+
+            totalMajor: {
+                "Computer Science": 72,
+                "Computer Engineering": 9,
+                "Electrical Engineering" : 6,
+                "INformatics": 5,
+                "Mathematics and Computer Science": 1
+            },
+
+            totalEthnicity: {
+                "East Asian or Asian-American": 45,
+                "South Asian or Asian-American": 21,
+                "White or Euro-American": 14,
+                "Other": 8,
+                "LatinX or Hispanic-American": 2,
+                "Black, Afro-Caribbean, or African-American": 2
+            },
+
+            uwEthnicity: {
+                "East Asian or Asian-American": 48,
+                "South Asian or Asian-American": 22,
+                "White or Euro-American": 13,
+                "Other": 1,
+                "LatinX or Hispanic-American": 3,
+                "Black, Afro-Caribbean, or African-American": 1
+            }
+        }
+    },
+
+    2018: {
+
+    },
+
+    2017: {
+        stats: {
+            total: {
+                numApplicants: 1800,
+                acceptanceRate: 63,
+
+                genderMale: 70,
+                genderFemale: 28,
+                genderNonBinary: 2,
+                
+                studentFirstGen: 26,
+                studentReturning: 21,
+
+                yearFreshman: 30,
+                yearSophomore: 32,
+                yearJunior: 24,
+                yearSenior: 12
+            },
+
+            uw: {
+                genderMale: 69,
+                genderFemale: 30,
+                genderNonBinary: 1,
+
+                acceptanceRate: 79
+            },
+
+            attendees: {
+                school: {
+                    "University of Washington" : 49,
+                    "University of British Columbia": 4,
+                    "UC Berkeley" : 2,
+                    "University of Waterloo": 2,
+                    "University of Florida" : 2,
+                },
+
+                totalMajor: {
+                    "Computer Science": 44, 
+                    "Computer Engineering": 6,
+                    "Informatics": 6,
+                    "Eletrical Engineering" : 2,
+                    "Software Engineering" : 2
+                },
+
+                totalEthnicity: {
+                    "East Asian or Asian-American": 37,
+                    "South Asian or Asian-American": 26,
+                    "White or Euro-American": 21,
+                    "Other": 4,
+                    "LatinX or Hispanic-American": 6,
+                    "Black, Afro-Caribbean, or African-American": 2,
+                    "Middle Eastern or North African": 2,
+                    "Hawaiian or Pacific Islander": 1,
+                    "Native American or Alaskan Native": 0
+                },
+
+                uwEthnicity: {
+                    "East Asian or Asian-American": 42,
+                    "South Asian or Asian-American": 22,
+                    "White or Euro-American": 24,
+                    "Other": 4,
+                    "LatinX or Hispanic-American": 3,
+                    "Black, Afro-Caribbean, or African-American": 1,
+                    "Middle Eastern or North African": 1,
+                    "Hawaiian or Pacific Islander": 2,
+                    "Native American or Alaskan Native": 0
+                }
+            }
+        }
+    }
+}
