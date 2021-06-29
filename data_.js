38967da25955aa7db03a27dfdb8891a9bd43a9a7
nathan-yan <nathancy@uw.edu>
2021-06-28 17:44:19 -0700

    Finished inputting all years' data

--- data_.js
+++ data_.js
@@ -55,14 +55,14 @@ const data = {
                 "Design" : 5.56
             },
 
-            totalCareerInterest: {
+            careers: {
                 "Software Engineering": 80.7,
                 "Data Science": 34.34, 
                 "Entrepreneurship": 23.22,
                 "Design" : 33.3,
                 "PM": 29.38,
                 "Consulting": 10.5
-            }
+            },
         }
     },
 
@@ -107,7 +107,7 @@ const data = {
                 "Computer Science": 72,
                 "Computer Engineering": 9,
                 "Electrical Engineering" : 6,
-                "INformatics": 5,
+                "Informatics": 5,
                 "Mathematics and Computer Science": 1
             },
 
@@ -132,7 +132,74 @@ const data = {
     },
 
     2018: {
+        stats: {
+            total: {
+                numApplicants: 1300,
+                acceptanceRate: 63, 
 
+                studentFirstGen: 24,
+                studentReturning: 20,
+
+                genderMale: 49,
+                genderFemale: 23,
+                genderNonBinary: 1,
+
+                yearFreshman: 31,
+                yearSophomore: 30,
+                yearJunior: 23,
+                yearSenior: 14,
+            },
+
+            uw: {
+                genderMale: 62,
+                genderFemale: 35,
+                genderNonBinary: 1,
+
+                acceptanceRate: 74
+            },
+        },
+
+        attendees: {
+            school: {
+                "University of Washington": 55,
+                "University of British Columbia": 8,
+                "UC Berkeley": 2,
+                "University of Southern California": 1,
+                "University of Texas - Austin": 1
+            },
+
+            totalMajor: {
+                "Computer Science": 43,
+                "Informatics": 7,
+                "Computer Engineering": 5,
+                "Pre-Science": 1,
+                "Electrical Engineering": 1
+            },
+
+            uwEthnicity: {
+                "East Asian or Asian-American": 49,
+                "South Asian or Asian-American": 21,
+                "White or Euro-American": 14,
+                "Other": 4,
+                "LatinX or Hispanic-American": 3,
+                "Black, Afro-Caribbean, or African-American": 1,
+                "Middle Eastern or North African": 1,
+                "Hawaiian or Pacific Islander": 1,
+                "Native American or Alaskan Native": 6
+            }, 
+
+            totalEthnicity: {
+                "East Asian or Asian-American": 43,
+                "South Asian or Asian-American": 23,
+                "White or Euro-American": 14,
+                "Other": 5,
+                "LatinX or Hispanic-American": 4,
+                "Black, Afro-Caribbean, or African-American": 2,
+                "Middle Eastern or North African": 2,
+                "Hawaiian or Pacific Islander": 1,
+                "Native American or Alaskan Native": 0
+            }
+        }
     },
 
     2017: {
@@ -161,48 +228,165 @@ const data = {
 
                 acceptanceRate: 79
             },
+        },
 
-            attendees: {
-                school: {
-                    "University of Washington" : 49,
-                    "University of British Columbia": 4,
-                    "UC Berkeley" : 2,
-                    "University of Waterloo": 2,
-                    "University of Florida" : 2,
-                },
+        attendees: {
+            school: {
+                "University of Washington" : 49,
+                "University of British Columbia": 4,
+                "UC Berkeley" : 2,
+                "University of Waterloo": 2,
+                "University of Florida" : 2,
+            },
 
-                totalMajor: {
-                    "Computer Science": 44, 
-                    "Computer Engineering": 6,
-                    "Informatics": 6,
-                    "Eletrical Engineering" : 2,
-                    "Software Engineering" : 2
-                },
+            totalMajor: {
+                "Computer Science": 44, 
+                "Computer Engineering": 6,
+                "Informatics": 6,
+                "Eletrical Engineering" : 2,
+                "Software Engineering" : 2
+            },
 
-                totalEthnicity: {
-                    "East Asian or Asian-American": 37,
-                    "South Asian or Asian-American": 26,
-                    "White or Euro-American": 21,
-                    "Other": 4,
-                    "LatinX or Hispanic-American": 6,
-                    "Black, Afro-Caribbean, or African-American": 2,
-                    "Middle Eastern or North African": 2,
-                    "Hawaiian or Pacific Islander": 1,
-                    "Native American or Alaskan Native": 0
-                },
+            totalEthnicity: {
+                "East Asian or Asian-American": 37,
+                "South Asian or Asian-American": 26,
+                "White or Euro-American": 21,
+                "Other": 4,
+                "LatinX or Hispanic-American": 6,
+                "Black, Afro-Caribbean, or African-American": 2,
+                "Middle Eastern or North African": 2,
+                "Hawaiian or Pacific Islander": 1,
+                "Native American or Alaskan Native": 0
+            },
 
-                uwEthnicity: {
-                    "East Asian or Asian-American": 42,
-                    "South Asian or Asian-American": 22,
-                    "White or Euro-American": 24,
-                    "Other": 4,
-                    "LatinX or Hispanic-American": 3,
-                    "Black, Afro-Caribbean, or African-American": 1,
-                    "Middle Eastern or North African": 1,
-                    "Hawaiian or Pacific Islander": 2,
-                    "Native American or Alaskan Native": 0
-                }
+            uwEthnicity: {
+                "East Asian or Asian-American": 42,
+                "South Asian or Asian-American": 22,
+                "White or Euro-American": 24,
+                "Other": 4,
+                "LatinX or Hispanic-American": 3,
+                "Black, Afro-Caribbean, or African-American": 1,
+                "Middle Eastern or North African": 1,
+                "Hawaiian or Pacific Islander": 2,
+                "Native American or Alaskan Native": 0
             }
         }
+    },
+
+    2016: {
+        stats: {
+            total: {
+                numApplicants: 1600,
+                acceptanceRate: 44,
+
+                genderMale: 71,
+                genderFemale: 26,
+                genderNonBinary: 2,
+
+                yearFreshman: 30,
+                yearSophomore: 30,
+                yearJunior: 26,
+                yearSenior: 10,
+
+                studentFirstGen: 27,
+                studentReturning: 18
+            },
+
+            uw: {
+                genderMale: 69,
+                genderFemale: 29,
+                genderNonBinary: 2,
+            }
+        }, 
+    
+
+        attendees: {
+            school: {
+                "University of Washington" : 41,
+                "University of British Columbia": 6,
+                "University of Florida" : 3,
+                "Purdue University": 2,
+                "UC Berkeley" : 2,
+            },
+
+            totalMajor: {
+                "Computer Science": 44, 
+                "Computer Engineering": 5,
+                "Informatics": 5,
+                "Eletrical Engineering" : 2,
+                "Software Engineering" : 1
+            },
+
+            uwEthnicity: {
+                "East Asian or Asian-American": 38,
+                "South Asian or Asian-American": 22,
+                "White or Euro-American": 25,
+                "Other": 6,
+                "LatinX or Hispanic-American": 3,
+                "Black, Afro-Caribbean, or African-American": 1,
+                "Middle Eastern or North African": 3,
+                "Hawaiian or Pacific Islander": 1,
+                "Native American or Alaskan Native": 0
+            },
+
+            totalEthnicity: {
+                "East Asian or Asian-American": 35,
+                "South Asian or Asian-American": 25,
+                "White or Euro-American": 24,
+                "Other": 6,
+                "LatinX or Hispanic-American": 5,
+                "Black, Afro-Caribbean, or African-American": 2,
+                "Middle Eastern or North African": 2,
+                "Hawaiian or Pacific Islander": 1,
+                "Native American or Alaskan Native": 0
+            }
+        }
+    },
+
+    2015: {
+        stats: {
+            total: {
+                numApplicants: 1500,
+                acceptanceRate: 57,
+
+                genderMale: 76, 
+                genderFemale: 22,
+                genderNonBinary: 1,
+
+                yearFreshman: 30, 
+                yearSophomore: 37,
+                yearJunior: 22,
+                yearSenior: 11,
+
+                studentReturning: 13
+            },
+
+            uw: {
+                genderMale: 71,
+                genderFemale: 26,
+                genderNonBinary: 1
+            }
+        },
+
+        attendees: {
+            school: {
+                "University of Washington" : 40,
+                "University of British Columbia": 8,
+                "Purdue University" : 3,
+                "University of Waterloo": 2,
+                "Georgia Institute of Technology": 2
+            },
+
+            totalMajor: {
+                "Computer Science": 45,
+                "Informatics": 6,
+                "Computer Engineering": 6,
+                "Electrical Engineering": 3,
+                "Software Engineering": 1
+            },
+
+
+        }
     }
+
 }
